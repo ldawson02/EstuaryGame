502197b55892f32ef0b98afa05850da44e535150
iheffner <iheffner@udel.edu>
2016-10-25 17:39:51 -0400

    Update Item.java

--- EstuaryGame/src/model/Item.java
+++ EstuaryGame/src/model/Item.java
@@ -1,8 +1,62 @@
 package model;
 
+/**
+ * Item is an abstract class that holds the position of an item in x-y coordinates
+ * 
+ * @author Lia Dawson
+ * @version 1.0
+ * @since 10/25/16
+ */
+
 public abstract class Item {
+
+	private int posX;
+	private int posY;
+	private int objectID;
+	
 	/**
-	 * Trying some shit
-	 * again
+	 * Constructor for Item
+	 * 
+	 * @param x	the initial x position of the Item
+	 * @param y	the initial y position of the Item
 	 */
+	public Item(int x, int y){
+		this.posX = x;
+		this.posY = y;
+	};
+	
+	/**
+	 * Constructor (no args)
+	 */
+	public Item(){};
+	
+	/**
+	 * Accesses and returns the x position of the Item
+	 * 
+	 * @return the x position of the Item
+	 */
+	public int getPosX(){
+		return posX;
+	};
+	
+	/**
+	 * Accesses and return the y position of the Item
+	 * 
+	 * @return the y position of the Item
+	 */
+	public int getPosY(){
+		return posY;
+	};
+	
+	/**
+	 * Allows for position to be updated
+	 * 
+	 * @param x the new x position of the Item
+	 * @param y the new y position of the item
+	 */
+	public void updatePos(int x, int y){
+		this.posX = x;
+		this.posY = y;
+	};
+	
 }
