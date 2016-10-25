3eba9a1dd378a03e344c95bd38e92850ba4118f1
Ian Heffner <iheffner@udel.edu>
2016-10-25 19:21:15 -0400

    CoastTest.java complete

--- EstuaryGame/src/test/model/TestCoast.java
+++ EstuaryGame/src/test/model/TestCoast.java
@@ -15,7 +15,7 @@ public class TestCoast {
 
 	public Coast coast;
 	int maxsize = 5;
-	int minsize = 5;
+	int minsize = 0;
 	double erosionrate = 1.0;
 	
 	@BeforeClass
@@ -55,7 +55,42 @@ public class TestCoast {
 			coast.erode();
 		}
 		assertEquals(coast.getSize(), minsize);
-		
+		//Size should not fall below minsize
+		coast.erode();
+		assertEquals(coast.getSize(), minsize);
 	}
-
+	
+	@Test
+	public void testRebuild() {
+		//Start at min size
+		coast.setSize(minsize);
+		assertEquals(coast.getSize(), minsize);
+		//Should increase by 1
+		coast.rebuild();
+		assertEquals(coast.getSize(), minsize + 1);
+		//Should increase again
+		coast.rebuild();
+		assertEquals(coast.getSize(), minsize + 2);
+		//Should be equal to minSize
+		while (coast.getSize() != maxsize) {
+			coast.rebuild();
+		}
+		assertEquals(coast.getSize(), maxsize);
+		//Size should not increase above maxsize
+		coast.rebuild();
+		assertEquals(coast.getSize(), maxsize);
+	}
+	
+	@Test 
+	public void testChangeErosionRate() {
+		//Should not have changed yet
+		assertTrue(coast.getErosionRate() == erosionrate);
+		//Test a positive change
+		coast.changeErosionRate(1.0);
+		assertTrue(coast.getErosionRate() == erosionrate + 1.0);
+		//Test a negative change
+		coast.setErosionRate(erosionrate);
+		coast.changeErosionRate(-0.5);
+		assertTrue(coast.getErosionRate() == erosionrate - 0.5);
+	}
 }
