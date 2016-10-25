9b339172fd19a390087c42faa14c573218000aa1
lsmei <esme@udel.edu>
2016-10-25 19:20:51 -0400

    Update healthbar

--- EstuaryGame/src/model/HealthBar.java
+++ EstuaryGame/src/model/HealthBar.java
@@ -1,11 +1,34 @@
 package model;
+	
+/**
+* The HealthBar class holds information about the health of the estuary and has the update method
+* 
+* @author Esme Li
+* @version 1.0
+* @since 10/25/16
+*/
 
 public class HealthBar extends Item {
-
 	private int maxHealth;
 	private int health;
 	
-	public void update(int points){};
+	/**
+	 * Constructs a HealthBar with 
+	 * @param max
+	 * @param health
+	 */
+	
+	public HealthBar(int max, int current){
+		super(max, current);
+	};
+	
+	/**
+	 * Updates the current health of the estuary
+	 * @param points
+	 */
+	public void update(int points) {
+		
+	};
 	
 	public int gethealth(){
 		return health;
