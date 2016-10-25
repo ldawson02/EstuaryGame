b994f43d8adf5026168a1f9d588f3d7012c7bf7b
rachelmuzzelo <rachelmuzzelo@wifi-roaming-128-4-98-94.host.udel.edu>
2016-10-25 17:14:29 -0400

    Barriers added

---
+++ EstuaryGame/src/model/Barrier.java
@@ -0,0 +1,14 @@
+package model;
+
+public abstract class Barrier implements Barriers {
+	int decayTime;
+	int health;
+	
+	/**
+	 * Constructor for Barrier
+	 */
+	public Barrier(){
+		
+	};
+
+}
---
+++ EstuaryGame/src/model/Barriers.java
@@ -0,0 +1,14 @@
+package model;
+
+public interface Barriers {
+	/**
+	 * author: rachel muzzelo
+	 * 10/25/16
+	 * Each barrier has build, decay, and crumble methods.
+	 */
+
+	void build();
+	void decay();
+	void crumble();
+
+}
