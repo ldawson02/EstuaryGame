6fd4ef0b9253301bfc37002cb1afd186a9971732
ldawson02 <ldawson02@yahoo.com>
2016-10-26 19:00:37 -0400

    non implemented stuff added

---
+++ EstuaryGame/src/controller/ActiveItems.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class ActiveItems {
+	//class that holds all our actual active items in an instance of the game
+	
+}
---
+++ EstuaryGame/src/controller/GameController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class GameController {
+
+	//the big shebang
+	
+}
