af0cfce8f60e3a4116b812539c9338704eba0179
wliapple <wliapple@wifi-roaming-128-4-92-5.host.udel.edu>
2016-10-26 17:50:01 -0400

    To be updated later when other classes done

---
+++ EstuaryGame/src/test/model/StormTest.java
@@ -0,0 +1,37 @@
+package test.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Storm;
+
+public class StormTest {
+
+	public Storm s;
+	
+	@Test
+	public void appearTest() {
+		fail("Not yet implemented");
+	}
+
+	
+	/*
+	 * should be able to get rid of gabions from the coasts by a certain amount-
+	 * either make them decay faster or get rid of a couple entirely
+	 */
+	@Test
+	public void destroyGabionsTest() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * create an AllDebris class which will have an added # of debris, and the types 
+	 * can be random
+	 */
+	
+	@Test
+	public void addTrashTest() {
+		fail("Not yet implemented");
+	}
+}
