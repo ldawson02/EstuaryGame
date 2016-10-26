65228d3beb79f0ea16a08807f24f546f1a0d61e3
lsmei <esme@udel.edu>
2016-10-26 16:56:38 -0400

    update storm

--- EstuaryGame/src/model/Storm.java
+++ EstuaryGame/src/model/Storm.java
@@ -1,8 +1,52 @@
 package model;
 
 public class Storm extends Item {
-
-	public void appear(){};
-	public void destroyGabions(){};
-	public void addTrash(){};
+	
+	/**
+	* Storm class- has appear(), destroyGabions(), and addTrash() methods
+	* 
+	* @author Esme Li
+	* @version 1.0
+	* @since 10/25/16
+	*/
+	
+	/**
+	 * Constructs a Storm 
+	 */
+	
+	private static int numDamage = 10;
+	private static int numTrash;
+	private int lifetime;
+	private int x = 0;
+	private int y = 0;
+	
+	public Storm() {
+		super();
+	}
+	
+	public void setLifetime(int x) {
+		this.lifetime = x;
+	}
+	
+	/**
+	 * Makes the storm in the game at x, y 
+	 */
+	public void appear() {
+		
+	};
+	
+	/**
+	 * Destroys the gabions at the set damage, numDamage
+	 */
+	public void destroyGabions() {
+		
+	};
+	
+	
+	/**
+	 * Adds a # of trash, numTrash
+	 */
+	public void addTrash() {
+		
+	};
 }
