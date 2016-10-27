1010e02d5c2e6fa0ec01ead4cef5a478b6994988
ldawson02 <ldawson02@yahoo.com>
2016-10-27 19:17:31 -0400

    edited controller

--- EstuaryGame/src/controller/ActiveItems.java
+++ EstuaryGame/src/controller/ActiveItems.java
@@ -1,6 +1,14 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.*;
 public class ActiveItems {
 	//class that holds all our actual active items in an instance of the game
+	Player mainPlayer;
+	ArrayList<Barriers> barriers;
+	ArrayList<Debris> debris;
+	HealthBar hBar;
+	ArrayList<Coast> coasts;
 	
 }
