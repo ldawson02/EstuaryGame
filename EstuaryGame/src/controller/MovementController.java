75ac4b56e78a632487e734ebac3c944e700df203
rachelmuzzelo <rachelmuzzelo@rachels-mbp.fios-router.home>
2016-10-31 18:11:03 -0400

    Movement controller update

--- EstuaryGame/src/controller/MovementController.java
+++ EstuaryGame/src/controller/MovementController.java
@@ -1,17 +1,29 @@
 package controller;
 
+import java.awt.geom.QuadCurve2D;
+
 public class MovementController {
-	public static int getStart(int vertex){
+	public static int getStart(int shift){
 		int xstart = 0;
-		if(vertex<=400){
-			xstart = -(150)^2-vertex;
+		if(shift <= 400){
+			xstart = 150+(-2/225)*(150)^2+shift;	
 		}
-		if(vertex<=400){
-			xstart = (150)^2 + vertex;
+		else if(shift > 400){
+			xstart = 400+(2/225)*(150)^2+shift;
 		}
-		return xstart;
+		return xstart;	
 	}
 	public static void move(model.Floater floater){
+		int newy = floater.getPosY() + floater.getSpeed();
+		int newx;
+		if(floater.getVertex() <= 400){
+			newx = 150+(-2/225)*(newy-150)^2+floater.getVertex();
+			floater.updatePos(newx, newy);
+		}
+		else if(floater.getVertex() > 400){
+			newx = 400+(2/225)*(newy-150)^2+floater.getVertex();
+			floater.updatePos(newx, newy);
+		}
 		
 		
 	}
