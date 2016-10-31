dfe4ea138587a15c1ff82cdaee067248270c7e61
rachelmuzzelo <rachelmuzzelo@rachels-mbp.fios-router.home>
2016-10-31 18:32:48 -0400

    Movement Controller update

--- EstuaryGame/src/controller/MovementController.java
+++ EstuaryGame/src/controller/MovementController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.awt.geom.QuadCurve2D;
 
 public class MovementController {
 	public static int getStart(int shift){
@@ -9,20 +8,25 @@ public class MovementController {
 			xstart = 150+(-2/225)*(150)^2+shift;	
 		}
 		else if(shift > 400){
-			xstart = 400+(2/225)*(150)^2+shift;
+			xstart = (2/225)*(150)^2+shift;
 		}
 		return xstart;	
 	}
 	public static void move(model.Floater floater){
-		int newy = floater.getPosY() + floater.getSpeed();
-		int newx;
-		if(floater.getVertex() <= 400){
-			newx = 150+(-2/225)*(newy-150)^2+floater.getVertex();
-			floater.updatePos(newx, newy);
+		if(floater.getPosY()<300){
+			int newy = floater.getPosY() + floater.getSpeed();
+			int newx;
+			if(floater.getVertex() <= 400){
+				newx = 150+(-2/225)*(newy-150)^2+floater.getVertex();
+				floater.updatePos(newx, newy);
+			}
+			else if(floater.getVertex() > 400){
+				newx = (2/225)*(newy-150)^2+floater.getVertex();
+				floater.updatePos(newx, newy);
+			}
 		}
-		else if(floater.getVertex() > 400){
-			newx = 400+(2/225)*(newy-150)^2+floater.getVertex();
-			floater.updatePos(newx, newy);
+		else if(floater.getPosY()>=300){
+			
 		}
 		
 		
