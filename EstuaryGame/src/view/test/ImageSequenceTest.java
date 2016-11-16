6f2eba3bf47950388bcc850a86fb8f3bf40fd882
Ian Heffner <iheffner@udel.edu>
2016-11-16 18:02:46 -0500

    ImageSequence fully functional, including testing

--- EstuaryGame/src/view/test/ImageSequenceTest.java
+++ EstuaryGame/src/view/test/ImageSequenceTest.java
@@ -2,6 +2,9 @@ package view.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import eNums.eAnimation;
@@ -10,8 +13,59 @@ import view.ImageSequence;
 public class ImageSequenceTest {
 
 	@Test
-	public void test() {
+	public void testSeq() {
 		ImageSequence seq = new ImageSequence(eAnimation.playerIdle);
+		assertEquals(seq.getSeq().size(), 3);
+		assertEquals(seq.getNumFrames(), 3);
 	}
-
+	
+	@Test
+	public void testSeqOneFrame() {
+		ImageSequence seq = new ImageSequence(eAnimation.playerLift);
+		assertEquals(seq.getSeq().size(), 1);
+		assertEquals(seq.getNumFrames(), 1);
+	}
+	
+	@Test
+	public void testDraw() {
+		ImageSequence seq = new ImageSequence(eAnimation.playerIdle);
+		ArrayList<BufferedImage> frames = seq.getSeq();
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(0));
+	}
+	
+	@Test
+	public void testDrawWithDelay() {
+		ImageSequence seq = new ImageSequence(eAnimation.playerIdle);
+		seq.setFrameDelay(1);
+		ArrayList<BufferedImage> frames = seq.getSeq();
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(1));
+		
+		seq.reset();
+		seq.setFrameDelay(2);
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(1));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(2));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(0));
+		assertEquals(seq.draw(), frames.get(1));
+	}
+	
 }
