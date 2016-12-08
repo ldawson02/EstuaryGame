414f7a7078e8636b044f0ba252bc1b381d60ab06
Ian Heffner <iheffner@udel.edu>
2016-12-08 01:08:14 -0500

    storm visuals not implemented

--- EstuaryGame/src/model/StormVisual.java
+++ EstuaryGame/src/model/StormVisual.java
@@ -2,8 +2,17 @@ package model;
 
 public class StormVisual extends Item {
 
+	private int speed;
+	
+	public static final int defaultHeight = 200;
+	public static final int defaultWidth = 250;
+	
 	public StormVisual(int x, int y) {
 		this.updatePos(x, y);
+	}
+	
+	public void move() {
+		updatePos(this.getPosX() + speed, this.getPosY() + speed);
 	}
 	
 }
