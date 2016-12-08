36cf4dc66e16a097b10c2522383acf3a680962e1
Ian Heffner <iheffner@udel.edu>
2016-12-08 01:50:31 -0500

    storm visual implemented

--- EstuaryGame/src/model/StormVisual.java
+++ EstuaryGame/src/model/StormVisual.java
@@ -2,17 +2,18 @@ package model;
 
 public class StormVisual extends Item {
 
-	private int speed;
+	private int speedx = 3;
+	private int speedy = 2;
 	
 	public static final int defaultHeight = 250;
 	public static final int defaultWidth = 250;
 	
 	public StormVisual(int x, int y) {
-		this.updatePos(x, y);
+		this.updatePos(-250, 600);
 	}
 	
 	public void move() {
-		updatePos(this.getPosX() + speed, this.getPosY() + speed);
+		updatePos(this.getPosX() + speedx, this.getPosY() - speedy);
 	}
 	
 }
